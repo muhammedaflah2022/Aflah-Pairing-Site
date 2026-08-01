0ba63e399950444352a159281b7590387189cb63
muhammedaflah2022 <githubhub533@gmail.com>
2026-08-01 11:54:33 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -52,14 +52,14 @@ router.get('/', async (req, res) => {
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                     await delay(800);
                     let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Arslan_Tech.sendMessage(Pair_Code_By_Arslan_Tech.user.id, { text: 'ARSLAN-MD~' + b64data });
+                    let session = await Pair_Code_By_Arslan_Tech.sendMessage(Pair_Code_By_Arslan_Tech.user.id, { text: 'Aflah-MD~' + b64data });
 
                     let Arslan_MD_TEXT = `
         
 ╔════════════════════◇
 ║『 SESSION CONNECTED』
-║ ✨ Arslan-MD 🔷
-║ ✨ ArslanMD OFFICIAL🔷
+║ ✨ Aflah-MD 🔷
+║ ✨ Aflah-MD OFFICIAL🔷
 ╚════════════════════╝
 
 
@@ -72,10 +72,10 @@ router.get('/', async (req, res) => {
 ╚════════════════════╝
 ╔════════════════════◇
 ║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
-║❍ 𝐎𝐰𝐧𝐞𝐫: 923237045919
-║❍ 𝐑𝐞𝐩𝐨: https://github.com/Arslan-MD/Arslan_MD
-║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: https://chat.whatsapp.com/KRyARlvcUjoIv1CPSSyQA5?mode=wwt
-║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306
+║❍ 𝐎𝐰𝐧𝐞𝐫: 919605046174
+║❍ 𝐑𝐞𝐩𝐨: 
+║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: 
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: 
 ║
 ║ ☬ ☬ ☬ ☬
 ╚═════════════════════╝
